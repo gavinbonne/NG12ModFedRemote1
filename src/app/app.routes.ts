2219b97f7a35f6f59946c5f7c541a07d646fcffd
gavinbonne <gbonnevi@gmail.com>
2021-06-01 17:26:33 -0500

    Fixed the billing sub routing issue

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { ArticleTileComponent } from './article-tile/article-tile.component';
 import { HealthTrackerComponent } from './health-tracker/health-tracker.component';
 import { SurveyTileComponent } from './survey-tile/survey-tile.component';
-import { BillingComponent } from './billing/billing.component';
 
 export const APP_ROUTES: Routes = [
     { path: '', redirectTo: 'billing', pathMatch: 'full'},
-    { path: 'billing', component: BillingComponent, pathMatch: 'full'},
+    {
+        path: 'billing',
+        loadChildren: () => import('./billing/billing.module').then(m => m.BillingModule)
+    },
     { path: 'article-tile', component: ArticleTileComponent, pathMatch: 'full'},
     { path: 'survey-tile', component: SurveyTileComponent, pathMatch: 'full'},
     { path: 'health-tracker', component: HealthTrackerComponent, pathMatch: 'full'},
